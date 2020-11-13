2c7d804bd85fa986a6cf3f8d5bbec917f550a214
Rego De Franca, Jose Luiz <joseluiz.regodefranca@ge.com>
2020-11-12 18:03:18 -0800

    Multiple XML files based on the the number of features in the JSON
    This version generates multiple XML files based on the the number of features in the JSON.

--- cucumber_json_to_junit_xml/cucumber_json_to_junit.py
+++ cucumber_json_to_junit_xml/cucumber_json_to_junit.py
@@ -10,110 +10,121 @@ def main():
     '''
     Main function
     '''
-    if len(sys.argv) < 2 or not sys.argv[1].endswith(".json"):
-        print("No JSON file path received. Usage:")
-        print("\tpython cucumber-json-to-junit.py source-report.json target-report.xml")
+    if len(sys.argv) < 2:
+        print("A JSON file needs to be provided as a parameter. Usage:")
+        print("\tpython3 cucumber-json-to-junit.py source-report.json")
         sys.exit()
-    if len(sys.argv) < 3 or not sys.argv[2].endswith(".xml"):
-        print("No XML file path received. Usage:")
-        print("\tpython cucumber-json-to-junit.py source-report.json target-report.xml")
+
+    if len(sys.argv) > 2:
+        print("A JSON file needs to be provided as a parameter. Usage:")
+        print("\tpython3 cucumber-json-to-junit.py source-report.json")
+        sys.exit()
+
+    if len(sys.argv) == 2 and not sys.argv[1].endswith(".json"):
+        print("Wrong file format provided as a parameter. Usage:")
+        print("\tpython3 cucumber-json-to-junit.py source-report.json")
         sys.exit()
 
     with open(sys.argv[1], "r") as json_file:
         print("Opening file " + sys.argv[1] + " in read-only")
         json_data = json.load(json_file)
 
-    test_cases = ""
     header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n"
-
-    test_suite_time = 0.0
-    failure_count = 0
-    scenario_count = 0
 
     for feature in json_data:
 
         feature_name = sanitize(feature["name"])
-        scenarios = feature["elements"]
-        feature_time = 0.0
+        xml_feature_name = (feature["name"].replace(" ","" )) + ".xml"
 
+        scenarios = feature["elements"]
+        test_suite_time = 0.0
+        feature_time = 0.0
+        scenario_count = 0
+        failure_count = 0
+
+        test_cases = ""
 
         for scenario in scenarios:
 
-            scenario_count += 1
-
-            scenario_name = sanitize(scenario["name"])
-            steps_blob = "<![CDATA["
-            err_blob = ""
-            scenario_status = "passed"
-            scenario_time = 0.0
-
             if scenario["type"] != "background":
+
+                scenario_name = sanitize(scenario["name"])
+                steps_blob = "<![CDATA["
+                err_blob = ""
+                scenario_status = "passed"
+
+                scenario_time = 0.0
+                scenario_count += 1
+            
                 for tag in scenario["tags"]:
                     steps_blob += tag["name"] + " "
                 steps_blob += "\n"
 
-            for step in scenario["steps"]:
+                for step in scenario["steps"]:
 
-                description = sanitize(step["name"])
-                results = step["result"]
-                status = sanitize(results["status"])
-                keyword = sanitize(step["keyword"])
+                    description = sanitize(step["name"])
+                    results = step["result"]
+                    status = sanitize(results["status"])
+                    keyword = sanitize(step["keyword"])
 
-                if status != "skipped":
-                    scenario_time += float(results["duration"]) / 1000000000
+                    if status != "skipped":
+                        scenario_time += float(results["duration"]) / 1000000000
 
-                num_dots = 83 - len(keyword) - len(description) - len(status)
-                if num_dots <= 0:
-                    num_dots = 1
+                    num_dots = 120 - len(keyword) - len(description) - len(status)
+                    if num_dots <= 0:
+                        num_dots = 1
 
-                steps_blob += keyword + description
-                for i in range(num_dots):
-                    steps_blob += "."
-                steps_blob += status + "\n"
-                if status == "failed":
-                    err_blob = sanitize(results["error_message"])
-                    scenario_status = "failed"
-                    failure_count += 1
+                    steps_blob += keyword + description
+                    for i in range(num_dots):
+                        steps_blob += "."
+                    steps_blob += status + "\n"
 
-            feature_time += scenario_time
+                    if status == "failed":
+                        err_blob = results["error_message"].replace("\"","&quot;")
+                        scenario_status = "failed"
+                        failure_count += 1
 
-            steps_blob += "]]>"
+                steps_blob += "]]>"
 
-            test_case = "<testcase "
-            test_case += "classname=\"" + feature_name + "\" "
-            test_case += "name=\"" + scenario_name + "\" "
-            test_case += "time=\"" + str(scenario_time) + "\">"
-            if scenario_status == "passed":
-                test_case += "<system-out>" + steps_blob + "</system-out>\n"
-            else:
-                test_case += "<failure message=\"" + err_blob + "\">"
-                test_case += steps_blob + "</failure>\n"
-            test_case += "</testcase>\n"
+                test_case = "<testcase "
+                test_case += "classname=\"" + feature_name + "\" "
+                test_case += "name=\"" + scenario_name + "\" "
+                test_case += "time=\"" + str(scenario_time) + "\">"
+                if scenario_status == "passed":
+                    test_case += "<system-out>" + steps_blob + "</system-out>\n"
+                else:
+                    test_case += "<failure message=\"" + err_blob + "\">"
+                    test_case += steps_blob + "</failure>\n"
+                test_case += "</testcase>\n"
 
-        test_cases += test_case
+                test_cases += test_case
+                feature_time += scenario_time
+
         test_suite_time += feature_time
 
-    test_suite = "<testsuite "
-    test_suite += "failures=\"" + str(failure_count) + "\" "
-    test_suite += "name=\"Cucumber JSON to JUnit\" "
-    test_suite += "skipped=\"0\" "
-    test_suite += "tests=\"" + str(scenario_count) + "\" "
-    test_suite += "time=\"" + str(test_suite_time) + "\">\n"
-    for test_case in test_cases:
-        test_suite += test_case
-    test_suite += "</testsuite>"
+        test_suite = "<testsuite "
+        test_suite += "failures=\"" + str(failure_count) + "\" "
+        test_suite += "name=\"" + str(feature_name) + "\" "
+        test_suite += "skipped=\"0\" "
+        test_suite += "tests=\"" + str(scenario_count) + "\" "
+        test_suite += "time=\"" + str(test_suite_time) + "\">\n"
+        
+        for test_case in test_cases:
+            test_suite += test_case
+
+        test_suite += "</testsuite>"
 
 
-    with open(sys.argv[2], "w") as junit_file:
-        print("Writing to file " + sys.argv[2] + "...")
-        junit_file.write(header)
-        junit_file.write(test_suite)
+        with open(xml_feature_name, "w") as junit_file:
+            print("Writing to file " + xml_feature_name + " ...")
+            junit_file.write(header)
+            junit_file.write(test_suite)
 
     print("Done.")
 
 def sanitize(input):
-    input = input.replace("&","&amp;").replace("\"","&quot;")
-    input = input.replace("<","&lt;").replace(">","&gt;")
+    #input = input.replace("&","&amp;").replace("\"","&quot;")
+    #input = input.replace("<","&lt;").replace(">","&gt;")
     return input
 
 if __name__ == "__main__":
